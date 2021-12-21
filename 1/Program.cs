a094517a5fd762642bafdd1faf9f66b5a29bbdd5
Gromova Nadya <nadia.gromova2011@yandex.ru>
2021-12-21 13:17:35 +0300

    1. По двум заданным числам проверять является ли первое квадратом второго

---
+++ 1/Program.cs
@@ -0,0 +1,15 @@
+﻿//1. По двум заданным числам проверять является ли первое квадратом второго
+
+int numberA = 5;
+int numberB = 15;
+System.Console.WriteLine(Math.Pow(numberA,2) == numberB);
+
+if (Math.Pow(numberA,2) == numberB)
+{
+    Console.WriteLine ("true");
+}
+ else
+ {
+     Console.WriteLine ("false");
+ }
+
