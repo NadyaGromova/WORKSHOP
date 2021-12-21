3dbed2a59f2cd99a90446b1cfbc979a7ac0cf0fe
Gromova Nadya <nadia.gromova2011@yandex.ru>
2021-12-21 13:18:09 +0300

    2. Даны два числа. Показать большее и меньшее число

---
+++ 2/Program.cs
@@ -0,0 +1,20 @@
+﻿//2. Даны два числа. Показать большее и меньшее число
+
+int numberA = 7;
+int numberB = 4;
+int min = numberA;
+int max = numberA;
+
+if (numberB>max)
+{
+    max = numberB;
+}
+else
+{
+    min = numberB;
+}
+
+Console.WriteLine($"максимальное - {max}");
+Console.WriteLine($"минимальное - {min}");
+
+
