5f546ca5099842d7e766ca13f74ff63d8f52f2c0
Gromova Nadya <nadia.gromova2011@yandex.ru>
2021-12-21 13:20:23 +0300

    7. Показать числа от -N до N

---
+++ 7/Program.cs
@@ -0,0 +1,10 @@
+﻿//7. Показать числа от -N до N
+
+int numberA = 5;
+int startNumber = numberA * (-1);
+
+while (startNumber <= numberA)
+{
+    Console.WriteLine($"{startNumber}");
+    startNumber++;
+}
