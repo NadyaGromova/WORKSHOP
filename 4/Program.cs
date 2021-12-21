fc646946c92ef3d3abacae3df518a424461539eb
Gromova Nadya <nadia.gromova2011@yandex.ru>
2021-12-21 13:19:09 +0300

    4. Найти максимальное из трех чисел

---
+++ 4/Program.cs
@@ -0,0 +1,11 @@
+﻿// 4. Найти максимальное из трех чисел
+
+int numberA = 2;
+int numberB = 6;
+int numberC = 10;
+int max = numberA;
+
+if (numberB > max) max = numberB;
+if (numberC > max) max = numberC;
+
+Console.WriteLine(max);
