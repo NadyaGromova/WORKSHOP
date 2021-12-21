af4827491ead5e109f5b14fcafce60730cc9e4f6
Gromova Nadya <nadia.gromova2011@yandex.ru>
2021-12-21 13:22:36 +0300

    13. Выяснить, кратно ли число заданному, если нет, вывести остаток.

---
+++ 13/Program.cs
@@ -0,0 +1,11 @@
+﻿// 13. Выяснить, кратно ли число заданному, если нет, вывести остаток.
+
+int numberA = 361;
+int numberB = 6;
+int rest = numberA % numberB;
+
+if (rest == 0) Console.WriteLine($"число {numberA} кратно числу {numberB}");
+else
+{
+    Console.WriteLine(rest);
+}
