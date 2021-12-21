94ead0bab520755a62d0d6a59be366232b890fb0
Gromova Nadya <nadia.gromova2011@yandex.ru>
2021-12-21 13:20:01 +0300

    6. Выяснить является ли число чётным

---
+++ 6/Program.cs
@@ -0,0 +1,11 @@
+﻿// 6. Выяснить является ли число чётным
+
+int number = 7;
+if (number % 2 == 0)
+{
+    Console.WriteLine($"число {number} чётное");
+}
+else
+{
+    Console.WriteLine($"число {number} нечётное");
+}
