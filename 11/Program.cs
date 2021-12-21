a923b996669d503027ead68f95d537ff0f68099d
Gromova Nadya <nadia.gromova2011@yandex.ru>
2021-12-21 13:21:54 +0300

    11. Дано число из отрезка [10, 99]. Показать наибольшую цифру числа

---
+++ 11/Program.cs
@@ -0,0 +1,16 @@
+﻿// 11. Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+int N = new Random().Next(10, 100);
+Console.WriteLine(N);
+
+int tenth = N / 10;
+int unit = N % 10;
+
+if (tenth > unit)
+{
+    Console.WriteLine(tenth);
+}
+
+else
+{
+    Console.WriteLine(unit);
+}
