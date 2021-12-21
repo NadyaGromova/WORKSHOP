460a692a7890b0b6cbd699ce6e8828bdb51c2527
Gromova Nadya <nadia.gromova2011@yandex.ru>
2021-12-21 13:22:16 +0300

    12. Удалить вторую цифру трёхзначного числа

---
+++ 12/Program.cs
@@ -0,0 +1,9 @@
+﻿//12.Удалить вторую цифру трёхзначного числа
+
+int number = new Random().Next(100,1000);
+Console.WriteLine(number);
+
+int tenth = number/100;
+int unit = number%10;
+
+Console.Write($"{tenth}{unit}");
