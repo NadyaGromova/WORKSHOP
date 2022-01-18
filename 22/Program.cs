d4210cdaed88bf3574a71e2f63e03f0a13a4c52f
Gromova Nadya <nadia.gromova2011@yandex.ru>
2022-01-18 14:23:36 +0300

    23. Показать таблицу квадратов чисел от 1 до N

---
+++ 22/Program.cs
@@ -0,0 +1,26 @@
+﻿// 22. Найти расстояние между точками в пространстве 2D/3D
+
+//22. Найти расстояние между точками в пространстве 2D/3D
+//2D:
+// double Distance(double x1, double x2, double y1, double y2)
+// {
+//     double dis = 0;
+
+//     dis = Math.Sqrt((Math.Pow ((x2-x1),2))+(Math.Pow((y2-y1),2)));
+//     return dis;
+// }
+
+// Console.WriteLine(Distance (1,5,1,1));
+
+
+
+//3D:
+double Distance(double x1, double x2, double y1, double y2, double z1, double z2)
+{
+    double dis = 0;
+
+    dis = Math.Sqrt((Math.Pow((x2 - x1), 2)) + (Math.Pow((y2 - y1), 2)) + (Math.Pow((z2 - z1), 2)));
+    return dis;
+}
+
+Console.WriteLine(Distance(1,2,3,4,5,6));
---
+++ 23/Program.cs
@@ -0,0 +1,21 @@
+﻿// 23. Показать таблицу квадратов чисел от 1 до N
+
+int[] Fillarray(int numbers)
+{
+    int[] collection = new int[numbers];
+    for (int i = 0; i < collection.Length; i++)
+    {
+        collection[i] = i + 1;
+    }
+    return collection;
+}
+
+void PrintArray(int[] N)
+{
+    for (int count = 0; count < N.Length; count++)
+    {
+        Console.WriteLine($"{N[count]}^2 = {Math.Pow(N[count], 2)}");
+    }
+}
+
+PrintArray(Fillarray(10));
