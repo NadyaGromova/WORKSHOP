be24893daf02e5eef04f3759c36f2e597f84840a
Gromova Nadya <nadia.gromova2011@yandex.ru>
2022-01-18 14:17:16 +0300

    20. Задать номер четверти, показать диапазоны для возможных координат

---
+++ 20/Program.cs
@@ -0,0 +1,12 @@
+﻿// 20. Задать номер четверти, показать диапазоны для возможных координат
+string Result(int qua)
+{
+    string Demonstrate = $"В четверти {qua} X - принимает значения";
+    if (qua == 1 | qua == 4) Demonstrate += $" больше 0. Y - принимает значения";
+    if (qua == 3 | qua == 2) Demonstrate += $" меньше 0. Y - принимает значения ";
+    if (qua == 1 | qua == 2) Demonstrate += $" больше 0.";
+    if (qua == 3 | qua == 4) Demonstrate += $" меньше 0.";
+    return Demonstrate;
+}
+
+Console.WriteLine (Result(3));
