f23eb7d8a3b6770e583f47af984ca402e0baf22c
Gromova Nadya <nadia.gromova2011@yandex.ru>
2022-01-18 14:24:28 +0300

    24. Найти кубы чисел от 1 до N

---
+++ 24/Program.cs
@@ -0,0 +1,10 @@
+﻿// 24. Найти кубы чисел от 1 до N
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= number; i++)
+{
+    int result = i * i * i;
+    Console.WriteLine($"{i} в кубе = {result}");
+}
