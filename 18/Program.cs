4b96127b75db500383edc6ca6dc97a9bd91ad212
Gromova Nadya <nadia.gromova2011@yandex.ru>
2022-01-18 14:04:09 +0300

    18. Проверить истинность утверждения (X  Y) = X  Y

---
+++ 18/Program.cs
@@ -0,0 +1,11 @@
+﻿// Проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y
+bool Q(bool X, bool Y)
+{
+    return (!(X || Y)) == (!X && !Y);
+}
+
+Console.WriteLine(Q(true, true));
+Console.WriteLine(Q(false, true));
+Console.WriteLine(Q(true, false));
+Console.WriteLine(Q(false, false));
+
