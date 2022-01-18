8d95ead136e6857c3a6e65f9631d0d33325bdc4a
Gromova Nadya <nadia.gromova2011@yandex.ru>
2022-01-18 14:25:30 +0300

    25. Найти сумму чисел от 1 до А

---
+++ 25/Program.cs
@@ -0,0 +1,12 @@
+﻿// 25. Найти сумму чисел от 1 до А
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+
+for (int i = 1; i <= number; i++)
+{
+    result += i;
+}
+
+Console.WriteLine(result);
