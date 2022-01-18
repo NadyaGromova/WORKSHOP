79d479ac80b5b0ca10577ea771c0d7631dea4837
Gromova Nadya <nadia.gromova2011@yandex.ru>
2022-01-18 14:02:00 +0300

    16. Дано число обозначающее день недели. Выяснить является номер дня недели выходным

---
+++ 16/Program.cs
@@ -0,0 +1,14 @@
+﻿// 16. Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+
+Console.WriteLine("Введите число: ");
+int day = Convert.ToInt32(Console.ReadLine());
+
+if (day == 6 || day == 7)
+{
+    Console.WriteLine("выходной");
+}
+else
+{
+    Console.WriteLine("будни");
+}
+
