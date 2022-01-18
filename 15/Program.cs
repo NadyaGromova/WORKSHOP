8a204fd47143a6e5690b4bea55d076a0c242f12d
Gromova Nadya <nadia.gromova2011@yandex.ru>
2022-01-18 14:00:38 +0300

    15. Дано число. Проверить кратно ли оно 7 и 23

---
+++ 15/Program.cs
@@ -0,0 +1,12 @@
+﻿// 15. Дано число. Проверить кратно ли оно 7 и 23
+
+int number = 161;
+
+if (number%7 ==0 &&number%23==0)
+{
+    Console.WriteLine($"число {number} кратно 7 и 23");
+}
+else 
+{
+    Console.WriteLine($"число {number} не кратно 7 и 23");
+}
