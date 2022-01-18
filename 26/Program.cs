4cfcabf2baaa731c3a74180eb7019f7ba99663b5
Gromova Nadya <nadia.gromova2011@yandex.ru>
2022-01-18 14:26:22 +0300

    26. Возведите число А в натуральную степень B используя цикл

---
+++ 26/Program.cs
@@ -0,0 +1,16 @@
+﻿// 26. Возведите число А в натуральную степень B используя цикл
+
+Console.WriteLine("Введите число A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int result = numberA;
+
+for (int i = 1; i < numberB; i++)
+{
+    result *= numberA;
+}
+
+Console.WriteLine(result);
