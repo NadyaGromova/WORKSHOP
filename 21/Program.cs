afd007cb18dc2d6681c4f5430d6d80fdf1a4eb71
Gromova Nadya <nadia.gromova2011@yandex.ru>
2022-01-18 14:19:19 +0300

    21. Программа проверяет пятизначное число на палиндромом.

---
+++ 21/Program.cs
@@ -0,0 +1,13 @@
+﻿//21. Программа проверяет пятизначное число на палиндромом.
+bool result(int a)
+{
+    int b = a;
+    int number = 0;
+    while (a > 0)
+    {
+        number = number * 10 + a % 10;
+        a = a / 10;
+    }
+    return number == b;
+}
+Console.WriteLine($"pal = {result(12321)}");
