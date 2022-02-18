3fef7debb63a8ee4265fa6ae61acacfb9667227c
Gromova Nadya <nadia.gromova2011@yandex.ru>
2022-02-18 13:15:58 +0300

    27. Определить количество цифр в числе

---
+++ 27/Program.cs
@@ -0,0 +1,6 @@
+﻿// 27. Определить количество цифр в числе
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(Convert.ToString(number).Length);
